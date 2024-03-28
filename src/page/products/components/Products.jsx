bb360b7e392b6cad96d0c1c10913b946da61cfbc
s0if <saifalkomi@gmail.com>
2024-03-28 14:58:09 +0200

    edit bagination

--- src/page/products/components/Products.jsx
+++ src/page/products/components/Products.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { TokenContext } from '../../context/components/Token';
 import Loder from '../../../components/Loder';
 import { Bounce, toast } from 'react-toastify';
@@ -9,19 +9,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Products() {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
-    const { token, auth } = useContext(TokenContext)
+    const { token, auth } = useContext(TokenContext);
     const [numberPage, setNumberPage] = useState([]);
-    const [numberOfPage, setNumberOfPage] = useState(1)
-    const getProducts = async (Page) => {
+    const [numberOfPage, setNumberOfPage] = useState(1);
+    const totalPages = Math.ceil(products.total / 4);
+
+    const getProducts = async (page) => {
         setNumberOfPage(page);
         try {
             const { data } = await axios.get(
-                `${import.meta.env.VITE_API}/products?page=${Page}&limit=4`
+                `${import.meta.env.VITE_API}/products?page=${page}&limit=4`
             );
             setProducts(data);
             setLoading(false);
-        }
-        catch (error) {
+        } catch (error) {
             toast.warn(error.response.data.message, {
                 position: "top-right",
                 autoClose: 5000,
@@ -33,24 +34,23 @@ function Products() {
                 theme: "dark",
                 transition: Bounce,
             });
-        }
-        finally {
             setLoading(false);
         }
-    }
+    };
+
     const handelcahnge = async (productId) => {
         setLoading(true);
         try {
             const { data } = await axios.post(
-                `${import.meta.env.VITE_API}/cart`
-                , {
-                    productId
-                }, {
-                headers: {
-                    Authorization: `Tariq__${token}`
+                `${import.meta.env.VITE_API}/cart`,
+                { productId },
+                {
+                    headers: {
+                        Authorization: `Tariq__${token}`,
+                    },
                 }
-            })
-            if (data.message == "success") {
+            );
+            if (data.message === "success") {
                 toast.success('added to cart', {
                     position: "top-right",
                     autoClose: 5000,
@@ -63,8 +63,7 @@ function Products() {
                     transition: Bounce,
                 });
             }
-        }
-        catch (error) {
+        } catch (error) {
             toast.warn(error.response.data.message, {
                 position: "top-right",
                 autoClose: 5000,
@@ -76,40 +75,41 @@ function Products() {
                 theme: "dark",
                 transition: Bounce,
             });
-        }
-        finally {
+        } finally {
             setLoading(false);
         }
-    }
-
+    };
     const page = () => {
         if (products && products.total) {
-            const totalPages = products.total / 4;
             const pagesArray = [];
             for (let i = 1; i <= totalPages; i++) {
                 pagesArray.push(i);
             }
             setNumberPage(pagesArray);
+        } else {
+            console.log('else');
         }
-        else console.log('else')
     };
+
     useEffect(() => {
-        page()
-    }, [products])
+        page();
+    }, [products]);
+
     useEffect(() => {
-        getProducts(1)
-    }, [])
+        getProducts(1);
+    }, []);
+
     if (loading) {
-        return < Loder />
+        return <Loder />;
     }
-    console.log(numberOfPage)
+
     return (
         <>
             <div className="bg-Categores">
                 <div className={`d-flex flex-wrap container gap-xl-5 gap-lg-2 gap-sm-1 w-100 justify-content-center ${auth.id}`}  >
                     {products.products.length ?
                         products.products.map(product =>
-                            <div className={`card width-cart mt-2 mb-4 bg-prodect-cart opacity-20 product-shadow ${product._id}`} >
+                            <div className={`card width-cart mt-2 mb-4 bg-prodect-cart opacity-20 product-shadow ${product._id}`} key={product._id}>
                                 <img src={product.mainImage.secure_url} className={`card-img-top position-relative w-photo`} alt="product mainImage" />
                                 <div className="card-body">
                                     <h5 className="card-title text-white">{product.name}</h5>
@@ -122,33 +122,53 @@ function Products() {
                                 >
                                     add product
                                 </button>
-
                             </div>
-                        ) : <spam className="card-title fs-2 my-2 text-danger">empty product</spam>
+                        ) : <span className="card-title fs-2 my-2 text-danger">empty product</span>
                     }
                 </div>
             </div>
             <nav aria-label="Page navigation example">
                 <ul className="pagination">
-                    <li className="page-item">
-                        <button className="page-link" onClick={() => getProducts(numberOfPage - 1)} aria-label="Previous">
-                            <span aria-hidden="true">«</span>
-                        </button>
-                    </li>
-                    {numberPage.map(page => {
-                        return <li className="page-item"><button className="page-link" onClick={() => getProducts(page)}>{page}</button></li>
-                    })}
-                    <li className="page-item">
-                        <button className="page-link" onClick={() => getProducts(numberOfPage + 1)} aria-label="Next">
-                            <span aria-hidden="true">»</span>
-                        </button>
-                    </li>
+                    {
+                        (numberOfPage == 1) ?
+                            <li className="page-item">
+                                <button className="page-link  disabled" aria-label="Previous">
+                                    <span aria-hidden="true">«</span>
+                                </button>
+                            </li> :
+                            <li className="page-item">
+                                <button className="page-link" onClick={() => getProducts(numberOfPage - 1)} aria-label="Previous">
+                                    <span aria-hidden="true">«</span>
+                                </button>
+                            </li>
+
+
+                    }
+
+                    {numberPage.map(page => (
+                        <li className="page-item" key={page}>
+                            <button className="page-link" onClick={() => getProducts(page)}>{page}</button>
+                        </li>
+                    )
+                    )
+                    }
+                    {
+                        (numberOfPage == totalPages) ?
+                            <li className="page-item">
+                                <button className="page-link" aria-label="Next">
+                                    <span aria-hidden="true">»</span>
+                                </button>
+                            </li> : <li className="page-item">
+                                <button className="page-link disabled" onClick={() => getProducts(numberOfPage + 1)} aria-label="Next">
+                                    <span aria-hidden="true">»</span>
+                                </button>
+                            </li>
+                    }
+
                 </ul>
             </nav>
-
-
         </>
-    )
+    );
 }
 
-export default Products
+export default Products;
