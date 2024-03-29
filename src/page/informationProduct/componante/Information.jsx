cb58db35cbed887c297a49b118b6c7bd82ac80ca
s0if <saifalkomi@gmail.com>
2024-03-29 18:31:16 +0300

    add information

--- src/page/informationProduct/componante/Information.jsx
+++ src/page/informationProduct/componante/Information.jsx
@@ -23,32 +23,44 @@ function Information() {
     console.log(product)
     return (
         <>
-            {<div className=' bg-prodect-information-cart'>
-                <div className='w-100 d-flex justify-content-center gap-5 py-5'>
-                    <img src={product.mainImage.secure_url} alt="photo" className='w-25 rounded border border-2 product-shadow' />
-                    <div className='w-50 border border-dark rounded h-50 bg-product-information mt-4'>
-                        <h1 className='border-bottom border-dark p-3 fs-2 px-3'>{product.name}</h1>
+            {
+                <div className=' bg-prodect-information-cart'>
+                    <div className='w-auto d-flex justify-content-center align-items-center gap-xl-5 gap-lg-2 gap-sm-1 py-5 flex-wrap flex-lg-nowrap  container'>
+                        <img src={product.mainImage.secure_url} alt="photo" className='rounded card-img-top product-shadow w-auto h-auto my-4' />
+                        <div className='w-auto border border-dark rounded h-auto bg-product-information'>
+                            <h1 className='border-bottom border-dark p-3 fs-2 px-3'>{product.name}</h1>
+                            {
+                                (product.finalPrice < product.price) ? <div className='d-flex gap-5 px-3'>
+                                    <h1 className='border-bottom border-dark p-3 fs-2 text-decoration-line-through '>{product.price}$</h1>
+                                    <h1 className='border-bottom border-dark p-3 fs-2 '>{product.finalPrice}$</h1>
+                                </div> :
+                                    <h1 className='border-bottom border-dark p-3 fs-2 '>{product.price}$</h1>
+                            }
+                            <p className='px-lg-3 p-sm-1 p-lg-3 px-sm-0 py-5 w-auto h-auto'>{product.description}</p>
+                            <div className="feedpack m-lg-3 m-sm-helf px-lg-3 p-sm-1 p-lg-3 px-sm-0 border border-danger rounded w-auto h-auto">
+                                <h1 className='border-bottom border-danger'>feedpack</h1>
+                                {
+                                    product.reviews.map(feedpack => (
+                                        <div className='d-flex align-items-center p-sm-1 px-lg-3 m-sm-helf m-lg-3 px-sm-0  col-lg-12 cpl-sm-4 flex-wrap bg-feedback w-auto h-auto '>
+                                            <h3>{feedpack.createdBy.userName}:</h3>
+                                            <h5 className='font-family '>{feedpack.comment}</h5>
+                                        </div>
+                                    )
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </div>
+                    <div className='d-flex d-flex justify-content-center gap-5 flex-wrap'>
                         {
-                            (product.finalPrice < product.price) ? <div className='d-flex gap-5 px-3'>
-                                <h1 className='border-bottom border-dark p-3 fs-2 text-decoration-line-through '>{product.price}$</h1>
-                                <h1 className='border-bottom border-dark p-3 fs-2 '>{product.finalPrice}$</h1>
-                            </div> :
-                                <h1 className='border-bottom border-dark p-3 fs-2 '>{product.price}$</h1>
+                            product.subImages.map(image =>
+                                <div className=' mb-5 aspect-ratio-21x9'>
+                                    <img src={image.secure_url} className='rounded product-shadow ' alt="image" />
+                                </div>
+                            )
                         }
-
-                        <p className='px-3 py-5'>{product.description}</p>
                     </div>
                 </div>
-                <div className='d-flex d-flex justify-content-center gap-5'>
-                    {
-                        product.subImages.map(image =>
-                            <div className='mb-5'>
-                                <img src={image.secure_url} className='rounded product-shadow h-auto' alt="" />
-                            </div>
-                        )
-                    }
-                </div>
-            </div>
             }
         </>
     )
